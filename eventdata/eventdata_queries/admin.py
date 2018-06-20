e4dbbb783e3764bf31a6c113d1f8c1f9da216c4a
Kripanshu Bhargava <kripanshubhargava@gmail.com>
2018-06-20 04:22:42 -0400

    issue #285 Models

--- eventdata/eventdata_queries/admin.py
+++ eventdata/eventdata_queries/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
-
+from eventdata_queries.models import (EventDataSavedQuery, ArchiveQueryJob)
 # Register your models here.
+
+class EventDataSavedQueryAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'description',
+                    'username',
+                    'result_count',
+                    'created',
+                    'modified',
+                    'saved_to_dataverse',
+                    'dataverse_url')
+
+
+
+
+admin.site.register(EventDataSavedQuery, EventDataSavedQueryAdmin)
+
+
+class ArchiveQueryJobAdmin(admin.ModelAdmin):
+    list_display = ('what',
+                    'saved_query',
+                    'status',
+                    'is_finished',
+                    'is_success',
+                    'message',
+                    'created',
+                    'modified')
+
+
+
+
+admin.site.register(ArchiveQueryJob, ArchiveQueryJobAdmin)
